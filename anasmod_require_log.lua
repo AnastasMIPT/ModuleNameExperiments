d545ab6792d8c6092f41f02589a6843efa2b05a4
AnastasMIPT <beliaev.ab@tarantool.org>
2021-11-22 16:54:05 +0300

    new aproach with require

---
+++ anasmod_require_log.lua
@@ -0,0 +1,36 @@
+-- anasmod_require_log
+local log = require("log_require")
+local clock = require('clock')
+
+local function make_logs(interations_num)
+    for i = 1, interations_num do
+        log.info("simle log from first module %d", i)
+    end
+end
+
+local function bench_make_logs(expirements_num, interations_num)
+    local sum = 0.0
+    local min_time = 100000000.0
+    local max_time = 0.0
+    for i = 1, expirements_num do
+        local time = clock.bench(make_logs, interations_num)[1]
+        sum = sum + time
+        if time < min_time then
+            min_time = time
+        end
+        if time > max_time then
+            max_time = time
+        end
+    end
+    local mean_time = (sum - min_time - max_time) / (expirements_num - 2)
+
+    return mean_time, max_time, min_time
+end
+
+
+local anasmod_require_log = {
+    make_logs       = make_logs,
+    bench_make_logs = bench_make_logs,
+}
+
+return anasmod_require_log
--- experiment.lua
+++ experiment.lua
@@ -1,11 +1,32 @@
 #!/usr/bin/env ../src/tarantool
 
+local function module_name_by_func(func_level)
+    local debug = debug or require('debug')
+    local name_of_need_module
+    local src_name = debug.getinfo(func_level + 1).short_src
+    name_of_need_module = src_name:match'.*/([^.]+)'
+    return name_of_need_module
+end
+
+old_require = require
+require = function(modname)
+    if modname == 'log_require' then
+        mod_name = module_name_by_func(2)
+        print(mod_name)
+        local log = old_require(modname)
+        log.new(mod_name)
+        return log
+    end
+    return old_require(modname)
+end
+
 box.cfg{log = '/dev/null'}
 
 
 local anasmod_my_log = require("anasmod_my_log")
 local anasmod_tur_log = require("anasmod_tur_log")
 local anasmod_vanil_log = require("anasmod_vanil_log")
+local anasmod_req_log = require("anasmod_require_log")
 local log = require("log")
 
 local clock = require('clock')
@@ -22,6 +43,8 @@ local mean_time_vanil, max_time_vanil, min_time_vanil      = anasmod_vanil_log.b
 local mean_time_my_mod_name, max_time_my_mod_name, min_time_my_mod_name =    anasmod_my_log.bench_make_logs_mod_name(expirements_num, iterations_num)
 local mean_time_my_file, max_time_my_file, min_time_my_file         =    anasmod_my_log.bench_make_logs_file(expirements_num, iterations_num)
 local mean_time_tur,   max_time_tur,   min_time_tur        =   anasmod_tur_log.bench_make_logs(expirements_num, iterations_num)
+local mean_time_req,   max_time_req,   min_time_req        =   anasmod_req_log.bench_make_logs(expirements_num, iterations_num)
+
 
 print('\ndefault log time')
 print('mean = ', mean_time_vanil, 'max = ', max_time_vanil, 'min = ', min_time_vanil)
@@ -32,9 +55,11 @@ print('mean = ', mean_time_my_mod_name, 'max = ', max_time_my_mod_name, 'min = '
 print('\ntime without changing code with just file')
 print('mean = ', mean_time_my_file, 'max = ', max_time_my_file, 'min = ', min_time_my_file)
 
-
 print("\ntime with Alexandr Turenko's approach")
 print('mean = ', mean_time_tur, 'max = ', max_time_tur, 'min = ', min_time_tur)
+
+print("\ntime with Igor Munkin's approach")
+print('mean = ', mean_time_req, 'max = ', max_time_req, 'min = ', min_time_req)
 
 
 print("\nwithout changing code    ", (mean_time_my_mod_name / mean_time_tur - 1)   * 100, " %    slower than Alexandr Turenko's approach")
@@ -45,6 +70,8 @@ print("default log                            ", iterations_num / mean_time_vani
 print("without changing code with module name ", iterations_num / mean_time_my_mod_name)
 print("without changing code with just file   ", iterations_num / mean_time_my_file)
 print("Alexandr Turenko's approach            ", iterations_num / mean_time_tur)
+print("Igor Munkin's approach            ", iterations_num / mean_time_req)
+
 
 box.schema.user.grant('guest', 'read,write,execute', 'universe', nil, {if_not_exists=true})
 
